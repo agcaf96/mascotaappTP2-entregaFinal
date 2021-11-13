e5d1b308b648eb02f76765e037b9ed416a003875
FedeMorgenstern <fede87morgan@gmail.com>
2021-11-13 16:53:58 -0300

    factory reporte y mails

--- src/modulos/reporteBuilder/index.js
+++ src/modulos/reporteBuilder/index.js
@@ -1,1 +1,7 @@
 import ReporteEnExcelBuilder from "./ReporteEnExcelBuilder.js";
+
+const creadorReporte = new ReporteEnExcelBuilder()
+
+export function getReporteExcelBuilder(){
+    return creadorReporte
+}
