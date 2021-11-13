58dce01a3d183b1e098ded1e0625e2bf6a2ab9eb
agcaf96 <a.cafiero@outlook.com>
2021-11-13 12:44:23 -0300

    test y config de ubicación

---
+++ test/testAgregarUbicacion.js
@@ -0,0 +1,23 @@
+import { asociarUbicacion}  from '../src/casosDeUso/asociarUbicacionAMascota.js'
+import { getDaoMascotas } from '../src/daos/DaoFactoryMascotas.js'
+
+const daoMascotas = getDaoMascotas()
+
+try {
+    const direccionDeOrigen = "Yatay 240, Almagro, Capital Federal, Argentina"
+    await asociarUbicacion(daoMascotas, 1, direccionDeOrigen)
+
+    const direccionDeOrigen2 = "Yatay 440, Almagro, Capital Federal, Argentina"
+    await asociarUbicacion(daoMascotas, 2, direccionDeOrigen2)
+    
+    const direccionDeOrigen3 = "Yatay 640, Almagro, Capital Federal, Argentina"
+    await asociarUbicacion(daoMascotas, 3, direccionDeOrigen3)
+
+    const m = await daoMascotas.buscar(1)
+    const m2 = await daoMascotas.buscar(2)
+    const m3 = await daoMascotas.buscar(3)
+
+    console.log(m)
+} catch (error) {
+    console.log(error)
+}
