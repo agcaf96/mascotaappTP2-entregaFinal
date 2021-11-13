65c86e26b27893f4f2b81e2fb663db44a55a87e3
florenciasoria <fsoria57@gmail.com>
2021-11-13 12:52:02 -0300

    Agrego config mail

---
+++ src/configEmail.js
@@ -0,0 +1,8 @@
+export const configuracionMail = {
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+        user: 'thalia.langosh57@ethereal.email',
+        pass: 'ctg6N6cwNpzhS4MmJZ'
+    }
+}
