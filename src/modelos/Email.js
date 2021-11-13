fc51d3d0e20cd3c01d74176e35f95439878b37c7
florenciasoria <fsoria57@gmail.com>
2021-11-13 12:43:33 -0300

    Agrego email modulo

---
+++ src/modelos/Email.js
@@ -0,0 +1,38 @@
+class Email {
+  constructor(destinatario, asunto, texto) {
+    this.setDestinario(destinatario);
+    this.setAsunto(asunto);
+    this.setText(texto)
+    this.from = "thalia.langosh57@ethereal.email"
+  }
+
+  setDestinario(destinario) {
+    if(!destinario || destinario == '') {
+      throw new Error("El destinario no puede ser vacio")
+    }
+    else {
+      this.to = destinario
+    }
+  }
+
+  setAsunto(asunto) {
+    if(!asunto || asunto == '') {
+      throw new Error("El asunto no puede ser vacio")
+    }
+    else {
+      this.subject = asunto
+    }
+  }
+
+  setText(texto) {
+    if(!texto || texto == '') {
+      throw new Error("El texto no puede ser vacio")
+    }
+    else {
+      this.text = texto
+    }
+  }
+  
+}
+  
+export default Email;
