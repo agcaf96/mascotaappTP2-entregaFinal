2bae1b5f433b953bb780d40abfe3771bf3586685
florenciasoria <fsoria57@gmail.com>
2021-11-14 19:29:07 -0300

    Validacion confirmarMail

--- src/casosDeUso/confirmarMailAlta.js
+++ src/casosDeUso/confirmarMailAlta.js
@@ -6,17 +6,13 @@ const daoPersonas = getDaoPersonas()
 const env = getEnviadorDeMails()
 
 async function confirmarRegistro(idPersona){
-
     const texto = "Registro Exitoso!"
     const asunto = "Confirmacion"
-    //Validar que exista la persona
-    const persona = await daoPersonas.buscar(idPersona)
-    const destinatario = persona.email
-    console.log(destinatario)
-
-    const email = new Email(destinatario, asunto, texto)
-
+    
     try {
+        const persona = await daoPersonas.buscar(idPersona)
+        const destinatario = persona.email
+        const email = new Email(destinatario, asunto, texto)
         await env.enviar(email)
     } catch(error){
         throw new Error()
