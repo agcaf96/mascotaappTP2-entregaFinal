e6b81c329f1a233d7d63eb517dd1d4c98beb595c
agcaf96 <a.cafiero@outlook.com>
2021-11-16 21:49:45 -0300

    Arreglé el try catch que está de mas y el let viejo

--- src/casosDeUso/agregoImagenMascota.js
+++ src/casosDeUso/agregoImagenMascota.js
@@ -3,14 +3,9 @@ import  { fromDTO, toDTO }  from '../modelos/Mascota.js'
 
 const daoMascotas = getDaoMascotas()
 export async function asociarFotoAMascota(idMascota, rutaImagen) {
-    try {
-        let mascotaBuscada = await daoMascotas.buscar(idMascota)   
-        const mascotaF = await(fromDTO(mascotaBuscada))
+        const mascotaBuscada = await daoMascotas.buscar(idMascota)   
+        const mascotaF = (fromDTO(mascotaBuscada))
         mascotaF.agregarImagen(rutaImagen)
-        const mascoNue = await daoMascotas.guardar(toDTO(mascotaF))
+        await daoMascotas.guardar(toDTO(mascotaF))
         return mascotaF;
-    } catch (error) {
-        throw error
-    }
-
 }
